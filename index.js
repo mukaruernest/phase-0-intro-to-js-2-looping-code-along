0b917ce04601e38b033b58446f363cc693206fea
Ernest <mukaruernesto@gmail.com>
2022-12-15 14:24:27 +0300

    looping lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(names,e="surprise"){
+    let newArray = []
+    for(let i = 0; i<names.length; i++){
+        newArray.push(console.log(`"Thank you, ${names[i]}, for the wonderful ${e} gift!"`))
+    }
+    return newArray;
+}
+writeCards(names,"surprise");
+
+
+
+function countDown(num) {
+    while (num >= 0) {
+    console.log(num);
+    num--;
+    }
+}
+countDown(10)
